9eb1ea78916afcfb67d3dbec7b1eeab0062d094f
Wiktor Jakubczyc <empikempik@gmail.com>
2016-06-23 10:59:03 +0200

    Add files via upload

---
+++ signkindlet.cmd
@@ -0,0 +1,4 @@
+rename MyWorld.jar MyWorld.azw2
+C:\"Program Files"\Java\jdk1.6.0_27\bin\jarsigner.exe -keystore developer.keystore -storepass password MyWorld.azw2 dktest
+C:\"Program Files"\Java\jdk1.6.0_27\bin\jarsigner.exe -keystore developer.keystore -storepass password MyWorld.azw2 ditest
+C:\"Program Files"\Java\jdk1.6.0_27\bin\jarsigner.exe -keystore developer.keystore -storepass password MyWorld.azw2 dntest
